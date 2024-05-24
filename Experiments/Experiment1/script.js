c28f36f023f1f49487bdfe681a8e19d2f748686e
Thomas Williams <tomjenswilliams@gmail.com>
2024-05-24 00:41:46 -0700

    Experiment 1 now working for a whole cube, in a tempermental way. Experiment 2 left the same as before. Experiment 3 left the same as before. Experiment 4 now has several drafts of log in related pages.

--- Experiments/Experiment1/script.js
+++ Experiments/Experiment1/script.js
@@ -1,20 +1,198 @@
 let leftButton = document.querySelector('#leftButton');
 let rightButton = document.querySelector('#rightButton');
-let leftSquare = document.querySelector('#leftSquare');
-let rightSquare = document.querySelector('#rightSquare');
+let upButton = document.querySelector('#upButton');
+let downButton = document.querySelector('#downButton');
+let theCube = document.querySelector('#cube');
+let firstSide = document.querySelector('#firstSide');
+let secondSide = document.querySelector('#secondSide');
+let thirdSide = document.querySelector('#thirdSide');
+let fourthSide = document.querySelector('#fourthSide');
+let fifthSide = document.querySelector('#fifthSide');
+let sixthSide = document.querySelector('#sixthSide');
+let sidesArray = [
+  null,
+  firstSide,
+  secondSide,
+  thirdSide,
+  fourthSide,
+  fifthSide,
+  sixthSide,
+];
+let leftRight = true;
+
+function Cube() {
+  this.front = 1;
+  this.left = 2;
+  this.right = 3;
+  this.top = 4;
+  this.bottom = 5;
+  this.back = 6;
+  //
+  this.turnLeft = function () {
+    [this.front, this.left, this.right, this.top, this.bottom, this.back] = [
+      this.right,
+      this.front,
+      this.back,
+      this.top,
+      this.bottom,
+      this.left,
+    ];
+  };
+  this.turnRight = function () {
+    [this.front, this.left, this.right, this.top, this.bottom, this.back] = [
+      this.left,
+      this.back,
+      this.front,
+      this.top,
+      this.bottom,
+      this.right,
+    ];
+  };
+  this.turnUp = function () {
+    [this.front, this.left, this.right, this.top, this.bottom, this.back] = [
+      this.bottom,
+      this.left,
+      this.right,
+      this.front,
+      this.back,
+      this.top,
+    ];
+  };
+  this.turnDown = function () {
+    [this.front, this.left, this.right, this.top, this.bottom, this.back] = [
+      this.top,
+      this.left,
+      this.right,
+      this.back,
+      this.front,
+      this.bottom,
+    ];
+  };
+  //
+  this.printCube = function () {
+    console.log(`
+      +-+
+      |${this.top}|
+    +-+-+-+
+    |${this.left}|${this.front}|${this.right}|
+    +-+-+-+
+      |${this.bottom}|
+      +-+
+      |${this.back}|
+      +-+
+    
+    `);
+  };
+}
+
+let myCube = new Cube();
+myCube.printCube();
 
 leftButton.addEventListener('click', () => {
   console.log('Left Clicked');
-  leftSquare.classList.remove('turnedAway');
-  leftSquare.classList.add('turnedToward');
-  rightSquare.classList.remove('turnedToward');
-  rightSquare.classList.add('turnedAway');
+  //
+  if (!leftRight) {
+    leftRight = true;
+    theCube.classList.toggle('updown');
+    theCube.classList.toggle('leftright');
+  }
+  //
+  theCube.insertBefore(
+    sidesArray[myCube.right],
+    theCube.children[0].nextSibling
+  );
+  sidesArray[myCube.front].classList.toggle('turningHorizontallyAway');
+  sidesArray[myCube.front].classList.toggle('full');
+  sidesArray[myCube.right].classList.toggle('shown');
+  sidesArray[myCube.right].classList.toggle('turningHorizontallyToward');
+  window.setTimeout(() => {
+    sidesArray[myCube.front].classList.toggle('shown');
+    sidesArray[myCube.right].classList.toggle('full');
+    sidesArray[myCube.front].classList.toggle('turningHorizontallyAway');
+    sidesArray[myCube.right].classList.toggle('turningHorizontallyToward');
+    theCube.append(sidesArray[myCube.front]);
+    //
+    myCube.turnLeft();
+    myCube.printCube();
+  }, 3000);
 });
 
 rightButton.addEventListener('click', () => {
   console.log('Right Clicked');
-  leftSquare.classList.remove('turnedToward');
-  leftSquare.classList.add('turnedAway');
-  rightSquare.classList.remove('turnedAway');
-  rightSquare.classList.add('turnedToward');
+  //
+  if (!leftRight) {
+    leftRight = true;
+    theCube.classList.toggle('updown');
+    theCube.classList.toggle('leftright');
+  }
+  //
+  theCube.insertBefore(sidesArray[myCube.left], theCube.children[0]);
+  sidesArray[myCube.front].classList.toggle('turningHorizontallyAway');
+  sidesArray[myCube.front].classList.toggle('full');
+  sidesArray[myCube.left].classList.toggle('shown');
+  sidesArray[myCube.left].classList.toggle('turningHorizontallyToward');
+  window.setTimeout(() => {
+    sidesArray[myCube.front].classList.toggle('shown');
+    sidesArray[myCube.left].classList.toggle('full');
+    sidesArray[myCube.front].classList.toggle('turningHorizontallyAway');
+    sidesArray[myCube.left].classList.toggle('turningHorizontallyToward');
+    theCube.append(sidesArray[myCube.front]);
+    myCube.turnRight();
+    myCube.printCube();
+  }, 3000);
+});
+
+upButton.addEventListener('click', () => {
+  console.log('Up Clicked');
+  //
+  if (leftRight) {
+    leftRight = false;
+    theCube.classList.toggle('updown');
+    theCube.classList.toggle('leftright');
+  }
+  //
+  theCube.insertBefore(
+    sidesArray[myCube.bottom],
+    theCube.children[0].nextSibling
+  );
+  sidesArray[myCube.front].classList.toggle('turningVerticallyAway');
+  sidesArray[myCube.front].classList.toggle('full');
+  sidesArray[myCube.bottom].classList.toggle('shown');
+  sidesArray[myCube.bottom].classList.toggle('turningVerticallyToward');
+  window.setTimeout(() => {
+    sidesArray[myCube.front].classList.toggle('shown');
+    sidesArray[myCube.bottom].classList.toggle('full');
+    sidesArray[myCube.front].classList.toggle('turningVerticallyAway');
+    sidesArray[myCube.bottom].classList.toggle('turningVerticallyToward');
+    theCube.append(sidesArray[myCube.front]);
+    //
+    myCube.turnUp();
+    myCube.printCube();
+  }, 3000);
+});
+
+downButton.addEventListener('click', () => {
+  console.log('Down Clicked');
+  //
+  if (leftRight) {
+    leftRight = false;
+    theCube.classList.toggle('updown');
+    theCube.classList.toggle('leftright');
+  }
+  //
+  theCube.insertBefore(sidesArray[myCube.top], theCube.children[0]);
+  sidesArray[myCube.front].classList.toggle('turningVerticallyAway');
+  sidesArray[myCube.front].classList.toggle('full');
+  sidesArray[myCube.top].classList.toggle('shown');
+  sidesArray[myCube.top].classList.toggle('turningVerticallyToward');
+  window.setTimeout(() => {
+    sidesArray[myCube.front].classList.toggle('shown');
+    sidesArray[myCube.top].classList.toggle('full');
+    sidesArray[myCube.front].classList.toggle('turningVerticallyAway');
+    sidesArray[myCube.top].classList.toggle('turningVerticallyToward');
+    theCube.append(sidesArray[myCube.front]);
+    //
+    myCube.turnDown();
+    myCube.printCube();
+  }, 3000);
 });
